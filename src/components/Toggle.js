43bec4dc47b1d5ce8b20889e7ad854d9bb030cf6
owenayora <ayoraowen@gmail.com>
2023-02-04 13:04:22 +0300

    Completed assignment

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 function Toggle() {
-  return <button>OFF</button>;
+  
+  const [isOn, setToggle]=useState(false)
+
+
+  function letsToggle(){
+    setToggle(!isOn)//or use cb function inside setToggle which is actually declared best practice in case u need previous value to determine updated value eg. when using count+1
+  }
+
+  const newColor= isOn?"red":"white"
+  
+
+  return <button style={{background:newColor}}onClick={letsToggle}>{isOn?"ON":"OFF"}</button>;
 }
 
 export default Toggle;
